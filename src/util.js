be24c517dced2d460e75c7f1fd8468b17570be60
ms <1318820477@qq.com>
2025-02-23 18:47:14 +0800

    fix bug

--- src/util.js
+++ src/util.js
@@ -1,22 +1,22 @@
 const dateMap = {
-  week: "last-week",
-  month: "last-month",
-  year: "last-year",
-};
+  week: 'last-week',
+  month: 'last-month',
+  year: 'last-year'
+}
 
 export const getDownload = async (packageName, dateKey) => {
-  const period = dateMap[dateKey];
+  const period = dateMap[dateKey]
   try {
     const response = await fetch(
       `https://api.npmjs.org/downloads/point/${period}/${packageName}`
-    );
+    )
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`HTTP error! Status: ${response.status}`)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error("Failed to fetch download data:", error.message);
-    throw error;
+    console.error('Failed to fetch download data:', error.message)
+    throw error
   }
-};
+}
